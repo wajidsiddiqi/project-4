9e6d214604c56e9e2b56545a389be708e66d0261
wajid <wajidsiddiqi12@gmail.com>
2023-10-06 22:14:20 +0500

    added navbar & btn

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,5 +1,34 @@
+"use client";
 import Image from "next/image";
+import { LogoText } from "@/app/styles/styles";
+import {
+  Navbar,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  NavbarBrand,
+} from "@nextui-org/react";
+import { ConnectKitButton } from "connectkit";
 
 export default function Home() {
-  return <div>Hi</div>;
+  return (
+    <Navbar position="static">
+      <NavbarBrand>
+        <LogoText>Decentralized Lottery</LogoText>
+      </NavbarBrand>
+      <NavbarContent justify="end">
+        <NavbarItem>
+          <ConnectKitButton.Custom>
+            {({ show, isConnected }) => {
+              return (
+                <Button onClick={show} color="primary" variant="flat">
+                  {isConnected ? "Disconnect" : "Connect"}
+                </Button>
+              );
+            }}
+          </ConnectKitButton.Custom>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
