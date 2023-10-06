ce2f9ae3ddadd82a6b44be3840c3e016f334fa49
wajid <wajidsiddiqi12@gmail.com>
2023-10-06 11:38:26 +0500

    setting theme colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,28 @@
-const { nextui } = require("@nextui-org/react");
+import { nextui } from "@nextui-org/theme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            background: "#e6f1fe", // or DEFAULT
+            foreground: "#11181C", // or 50 to 900 DEFAULT
+            primary: "#006FEE", // Blue
+          },
+        },
+      },
+    }),
+  ],
 };
