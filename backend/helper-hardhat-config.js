98e61d6d3a5eeedca625fab17fbb1d79201a2db9
wajid <wajidsiddiqi12@gmail.com>
2023-10-10 19:10:28 +0500

    added sub id

--- backend/helper-hardhat-config.js
+++ backend/helper-hardhat-config.js
@@ -7,7 +7,7 @@ const networkConfig = {
     entranceFee: ethers.utils.parseEther("0.01"),
     gasLane:
       "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c",
-    subscriptionId: "2430",
+    subscriptionId: "5826",
     callbackGasLimit: "500000",
     interval: "30",
   },
